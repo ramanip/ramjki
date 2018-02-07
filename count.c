be5c09aafb27b0b4a2d8b7c5dad83e57f0437a11
ramanip <31653594+ramanip@users.noreply.github.com>
2018-02-07 15:53:23 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+char ch;
+unsigned in long word count;
+int 0;
+word count=0=0;
+{if(ch==1'//ch=='1')
+{
+++word count;
+}
+printf("%\n %ln % ln ln|n"word count");
+return 0;
+}
