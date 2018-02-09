ba991b55af0b429d4b69812d3ef7a2b88e332cad
ramanip <31653594+ramanip@users.noreply.github.com>
2018-02-09 14:13:32 +0530

    Rename space.c to sai.c

---
+++ sai.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch[200];
+scanf("%s",a);
+int l=0,s,i;
+s=strlen(0)
+for(i=0;i<=s;i++);
+{
+if(isalphaa[i])
+l++;
+printf("%d",l);
+return 0;
+}
