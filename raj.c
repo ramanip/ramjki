b0a3a29a97653c12aae19b61eb5546758dbad9bf
ramanip <31653594+ramanip@users.noreply.github.com>
2018-02-09 15:02:04 +0530

    Create raj.c

---
+++ raj.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char string[50];
+int a,count=0;
+scanf("%s",&string[a]);
+while((string[i]));
+{
+if((string[a]>='0')&&(string[a]<='9'))
+{
+count+=1;
+}
+a++;
+}
+printf("%d",count);
+}
