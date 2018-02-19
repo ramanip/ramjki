d4e0f893ba3df6b1748376c8170a3b041cc68788
ramanip <31653594+ramanip@users.noreply.github.com>
2018-02-19 16:55:19 +0530

    Rename count.c to ram.c

---
+++ ram.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[10]="ramani";
+    int i,times;
+    scanf("%d",&times);
+    for(i=0;i<times;i++)
+    {
+        printf("%s\n",a);
+    }
+}
