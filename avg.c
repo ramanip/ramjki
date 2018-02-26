7cafb59a69af730a5867806b690a4a226854f65b
ramanip <31653594+ramanip@users.noreply.github.com>
2018-02-26 16:56:01 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,11 @@
+
+#include<conio.h>
+int main()
+{
+    int a,b,c,d,e,f,avg;
+    printf("enter the number");
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+    f=a+b+c+d+e;
+    avg=f/5;
+    printf("%d",avg);
+}
