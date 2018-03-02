4c410dbc44646296981ba29962c1f9ef10a62844
ramanip <31653594+ramanip@users.noreply.github.com>
2018-03-02 16:39:37 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int s;
+  scanf("%d",&s);
+  switch(s)
+  {
+      case 1: printf("one");
+      break;
+      case 2: printf("two");
+      break;
+      case 3: printf("three");
+      break;
+      case 4: printf("fore");
+      break;
+      case  5:printf("five");
+      break;
+  }
+}
