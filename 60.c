0127e7fbf345a4fe29674e12481cc9d476c5506a
ramanip <31653594+ramanip@users.noreply.github.com>
2018-03-16 16:44:00 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=6,b=7,c;
+    c=a+b;
+    printf("%d",c);
+    if(c%2==0)
+    {
+        printf("the number is even");
+    }
+    else
+    {
+        printf("the number is odd");
+    }
+}
